5df8ec027db8b3bda23d6d9b7dce9940689c2d17
Claire Rogers <claire@guiltydog.co.uk>
2021-12-25 22:22:49 +0000

    Handle statements.

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -14,10 +14,14 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         defineAst(outputDir, "Expr", Arrays.asList(
-           "Binary : Expr left, Token operator, Expr right",
-           "Grouping : Expr expression",
-           "Literal : Object value",
-           "Unary : Token operator, Expr right"
+            "Binary : Expr left, Token operator, Expr right",
+            "Grouping : Expr expression",
+            "Literal : Object value",
+            "Unary : Token operator, Expr right"
+        ));
+        defineAst(outputDir, "Stmt", Arrays.asList(
+            "Expression : Expr expression",
+            "Print : Expr expression"
         ));
     }
 
@@ -25,7 +29,7 @@ public class GenerateAst {
         String path = outputDir + "/" + baseName + ".java";
         PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-        writer.println("package com.craftinginterpreters.lox");
+        writer.println("package com.craftinginterpreters.lox;");
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
