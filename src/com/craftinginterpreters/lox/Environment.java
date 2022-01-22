3f6aed72e31040c6dabf8ef9f28a3b964aa50138
Claire Rogers <claire@guiltydog.co.uk>
2022-01-22 23:28:37 +0000

    Resolver

--- src/com/craftinginterpreters/lox/Environment.java
+++ src/com/craftinginterpreters/lox/Environment.java
@@ -1,5 +1,7 @@
 package com.craftinginterpreters.lox;
 
+import com.sun.tools.doclint.Env;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,5 +43,22 @@ class Environment {
 
     void define(String name, Object value) {
         values.put(name, value);
+    }
+
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+
+        return environment;
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
     }
 }
