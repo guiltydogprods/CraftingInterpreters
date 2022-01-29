b83022f0fd90dce5c96f9b2310ae7aeb63d257ae
Claire Rogers <claire@guiltydog.co.uk>
2022-01-29 22:27:54 +0000

    Finish basic chunk handling and add beginnings of Virtual Machine.

--- clox/clox/chunk.h
+++ clox/clox/chunk.h
@@ -2,8 +2,15 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+	OP_CONSTANT,
+	OP_ADD,
+	OP_SUBTRACT,
+	OP_MULTIPLY,
+	OP_DIVIDE,
+	OP_NEGATE,
 	OP_RETURN,
 } OpCode;
 
@@ -11,10 +18,13 @@ typedef struct {
 	int32_t count;
 	int32_t capacity;
 	uint8_t* code;
+	int32_t* lines;
+	ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int32_t line);
+int32_t addConstant(Chunk* chunk, Value value);
 
 #endif
